73d3b139bda8e6133651f0ec7872c26e6e8a6321
Ivanor1505 <ivanor1505@gmail.com>
2023-06-16 12:23:15 +0300

    8-10

--- src (3)/src/js/task-08.js
+++ src (3)/src/js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
 
+console.dir(loginForm);
+
+loginForm.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+        
+    if (!emailInput.value || !passwordInput.value) {
+    alert('Всі поля повинні бути заповнені');
+    return;
+    }
+    
+    const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+    };
+    
+console.log(formData);
+  loginForm.reset();
+}
--- src (3)/src/js/task-10.js
+++ src (3)/src/js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const selectors = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesContainer: document.querySelector('#boxes')
+}
+
+selectors.createBtn.addEventListener('click', createBoxes);
+selectors.destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = selectors.input.value;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i+=1){
+    const size = 30 + i * 10;
+    const div = document.createElement('div');
+
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxes.push(div);
+    console.log(boxes);
+  }
+  selectors.boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  selectors.boxesContainer.innerHTML = '';
+}
